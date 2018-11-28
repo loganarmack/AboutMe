dceeb26c8f9d5ebdfa7dc844b84a3b808e29972e
Logan Mack <loganarmack@gmail.com>
2018-11-27 21:44:59 -0500

    Added data binding to AboutMe

--- app/src/main/java/logan/example/com/aboutme/MainActivity.kt
+++ app/src/main/java/logan/example/com/aboutme/MainActivity.kt
@@ -15,22 +15,26 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val myName: MyName = MyName("Logan Mack")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
+
         binding.doneButton.setOnClickListener {
             addNickname(it)
         }
     }
 
     private fun addNickname(view: View) {
-
         binding.apply {
-            binding.nicknameText.text = binding.nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
             invalidateAll()
-            binding.nicknameEdit.visibility = View.GONE
+            nicknameEdit.visibility = View.GONE
             view.visibility = View.GONE
-            binding.nicknameText.visibility = View.VISIBLE
+            nicknameText.visibility = View.VISIBLE
         }
 
         //hides keyboard
